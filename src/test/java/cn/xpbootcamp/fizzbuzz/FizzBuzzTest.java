9fbd274df3620330f1ebfe842c7f001b1895bde2
Calidan <calidan@vip.qq.com>
2020-05-01 19:30:45 +0800

    add init code

---
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package cn.xpbootcamp.fizzbuzz;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FizzBuzzTest {
+
+    @Test
+    void testSay() {
+
+    }
+
+}
